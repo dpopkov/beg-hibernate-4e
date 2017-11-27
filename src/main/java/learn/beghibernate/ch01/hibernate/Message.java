d1a9171ae1b575c670a8b7182a444080886e3062
dpopkov <pkvdenis@gmail.com>
2017-11-27 23:34:00 +0300

    Hibernate Hello world example.

---
+++ src/main/java/learn/beghibernate/ch01/hibernate/Message.java
@@ -0,0 +1,45 @@
+package learn.beghibernate.ch01.hibernate;
+
+import javax.persistence.*;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
+
---
+++ src/test/java/learn/beghibernate/ch01/hibernate/PersistenceTest.java
@@ -0,0 +1,50 @@
+package learn.beghibernate.ch01.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PersistenceTest {
+    private static SessionFactory factory = null;
+
+    @BeforeClass
+    public static void setup() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    @Test
+    public void test01SaveMessage() {
+        Message message = new Message("Hello, world");
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(message);
+            tx.commit();
+        }
+    }
+
+    @Test
+    public void test02ReadMessage() {
+        try (Session session = factory.openSession()) {
+            List<Message> list = session.createQuery("from Message", Message.class).list();
+
+            assertEquals(1, list.size());
+            for (Message m : list) {
+                assertEquals("Hello, world", m.getText());
+                System.out.println(m.getText());
+            }
+        }
+    }
+}
