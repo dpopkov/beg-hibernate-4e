9a8ddd9d69a8b3aa6c96b91d24002237cf863070
dpopkov <pkvdenis@gmail.com>
2017-11-29 14:07:06 +0300

    Create simple model.

---
+++ ch03/src/main/java/learn/beghibernate/ch03/simple/Person.java
@@ -0,0 +1,23 @@
+package learn.beghibernate.ch03.simple;
+
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ ch03/src/test/java/learn/beghibernate/ch03/simple/ModelTest.java
@@ -0,0 +1,25 @@
+package learn.beghibernate.ch03.simple;
+
+import org.junit.Test;
+
+public class ModelTest {
+    @Test
+    public void testModelCreation() {
+        Person subject = new Person();
+        subject.setName("J. C. Smell");
+
+        Person observer = new Person();
+        observer.setName("Drew Lombardo");
+
+        Skill skill = new Skill();
+        skill.setName("Java");
+
+        Ranking ranking = new Ranking();
+        ranking.setSubject(subject);
+        ranking.setObserver(observer);
+        ranking.setSkill(skill);
+        ranking.setRanking(8);
+
+        System.out.println(ranking);
+    }
+}
