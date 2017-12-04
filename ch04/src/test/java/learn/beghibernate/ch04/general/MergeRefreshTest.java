861fe0f15056e89c4605327413d8bc50ed8271c5
dpopkov <pkvdenis@gmail.com>
2017-12-04 15:58:57 +0300

    Refreshing entities.

--- ch04/src/test/java/learn/beghibernate/ch04/general/MergeRefreshTest.java
+++ ch04/src/test/java/learn/beghibernate/ch04/general/MergeRefreshTest.java
@@ -8,21 +8,10 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class MergeRefreshTest {
+
     @Test
     public void testMerge() {
-        Long id;
-        try (Session session = SessionUtil.getSession()) {
-            Transaction tx = session.beginTransaction();
-
-            SimpleObject obj = new SimpleObject();
-            obj.setKey("testMerge");
-            obj.setValue(11L);
-            session.save(obj);
-
-            id = obj.getId();
-
-            tx.commit();
-        }
+        Long id = createAndSaveSimpleObject("testMerge", 11L);
 
         SimpleObject obj2 = validateSimpleObject(id, 11L);
         obj2.setValue(22L);
@@ -36,6 +25,41 @@ public class MergeRefreshTest {
         }
 
         validateSimpleObject(id, 22L);
+    }
+
+    @Test
+    public void testRefresh() {
+        Long id = createAndSaveSimpleObject("testMerge", 11L);
+
+        SimpleObject obj2 = validateSimpleObject(id, 11L);
+        obj2.setValue(22L);
+
+        try (Session session = SessionUtil.getSession()) {
+            Transaction tx = session.beginTransaction();
+
+            session.refresh(obj2);
+
+            tx.commit();
+        }
+
+        validateSimpleObject(id, 11L);
+    }
+
+    private Long createAndSaveSimpleObject(String key, Long value) {
+        Long objectId = null;
+        try (Session session = SessionUtil.getSession()) {
+            Transaction tx = session.beginTransaction();
+
+            SimpleObject obj = new SimpleObject();
+            obj.setKey(key);
+            obj.setValue(value);
+            session.save(obj);
+
+            objectId = obj.getId();
+
+            tx.commit();
+        }
+        return objectId;
     }
 
     private SimpleObject validateSimpleObject(Long id, Long value) {
